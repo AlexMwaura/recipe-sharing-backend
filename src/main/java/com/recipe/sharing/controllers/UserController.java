396df6e9cb0301147b5cfe3388a0ad6dee2a9905
Alex Mwaura <alexmwaura36@gmail.com>
2024-09-30 12:27:27 +0300

    cors configuration

--- src/main/java/com/recipe/sharing/controllers/UserController.java
+++ src/main/java/com/recipe/sharing/controllers/UserController.java
@@ -4,13 +4,11 @@ import com.recipe.sharing.dto.LoginRequest;
 import com.recipe.sharing.dto.LoginResponse;
 import com.recipe.sharing.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:4200")
 
 public class UserController {
     @Autowired
